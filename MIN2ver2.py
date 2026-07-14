1d7cf05de519a055879a56737b7d09916a8ffc56
fujitenmon-coder <Arikui57@outlook.jp>
2026-07-14 17:57:22 +0900

    solveERROR
    line123,129にて、for文中でイテラルオブジェクトを消去していた影響で、以下が発生しました。
    改善しました。
    
    [Running] python -u "c:\projects\seeing-fork_arikui\ffiinn--sseeiinngg\MIN2_ignore_sunspots.py"
    first circleNone
    only outside circleNone
    �O���̓_�̐�:9,�S�̂̓_�̐�:14
    x,y:(535, 441) min2far:251.75998763759867,y-cyo:50.124474653173934
    x,y:(537, 552) min2far:249.37867741306283,y-cyo:60.875525346826066
    x,y:(594, 662) min2far:191.5648954583974,y-cyo:170.87552534682607
    x,y:(643, 704) min2far:212.86056520432123,x-cxo:143.47799174590307
    x,y:(964, 472) min2far:255.987907534283,y-cyo:19.124474653173934
    Traceback (most recent call last):
      File "c:\projects\seeing-fork_arikui\ffiinn--sseeiinngg\MIN2_ignore_sunspots.py", line 146, in <module>
        print("cx,cy,r=",MIN2_ignore_sunspots(cv2.imread(picpath,0),filter_times=2,show=True,debug=True))
                         ~~~~~~~~~~~~~~~~~~~~^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "c:\projects\seeing-fork_arikui\ffiinn--sseeiinngg\MIN2_ignore_sunspots.py", line 115, in MIN2_ignore_sunspots
        x=spots[i][0]
          ~~~~~^^^
    IndexError: list index out of range

--- MIN2ver2.py
+++ MIN2ver2.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 最小二乗法による円の検出を行う関数を作成します。
 一度検出した近似円の内側にある点のうち、近似円の外側にある点だけから近似した円からlimbwigth*(3/2)の
 範囲にないもんは黒点とみなします。
-version 2.1
+version 2.1.1
 """
 
 def cut_and_sampling(sun_threshold):#imgの画像をnで分割、太陽の縁の点をsampling
@@ -98,7 +98,7 @@ def MIN2_ignore_sunspots(readed_img:np.ndarray,n=10,light_threshold=50,filter_ti
     spots=cut_and_sampling(light_threshold)#spots=[[x1,y1],[x2,y2],...]の形式で、縁の点の座標を格納したlist
     cx,cy,r=fit_circle(spots)#一回目の円情報
     print(f"first circle{show_circle(spots,(cx,cy,r))}") if debug and show else None#一回目の円を表示debug用
-    for i in  range(filter_times):#何度か繰り返すことでふくすうの黒点にも対応
+    for filtime in  range(filter_times):#何度か繰り返すことでふくすうの黒点にも対応
         #===MIN2の外側の点を抽出===
         outside_spots=[]
         for i in range(len(spots)):
@@ -112,22 +112,26 @@ def MIN2_ignore_sunspots(readed_img:np.ndarray,n=10,light_threshold=50,filter_ti
             #===外側の点の円からlimb_wigthの範囲にない内側の点を抽出===
             sunspot=False
             print (f"外側の点の数:{len(outside_spots)},全体の点の数:{len(spots)}") if debug else None
-            for i in range(len(spots)):
-                x=spots[i][0]
-                y=spots[i][1]
-                if not spots[i] in outside_spots:#内側の点だけ
+            pop_queue=[]#削除する点のindexを格納するlist
+            for i, spot in enumerate(spots):
+                x=spot[0]
+                y=spot[1]
+                if not spot in outside_spots:#内側の点だけ
                     if (x-cxo)**2 > (y-cyo)**2:#円のRLTBのうちRLなら、
                         min2far = np.sqrt(ro**2-(y-cyo)**2)
                         print(f"x,y:{x,y} min2far:{min2far},y-cyo:{np.abs(cyo-y)}") if debug else None
                         if min2far-np.abs(cxo-x) > limb_wigth:
                             sunspot=True
-                            spots.pop(i)
+                            pop_queue.append(i)
                     else:#円のRLTBのうちTBなら
                         min2far = np.sqrt(ro**2-(x-cxo)**2)
                         print(f"x,y:{x,y} min2far:{min2far},x-cxo:{np.abs(cxo-x)}") if debug else None
                         if min2far-np.abs(cyo-y) > limb_wigth:
                             sunspot=True
-                            spots.pop(i)
+                            pop_queue.append(i)
+            # 削除する点を一括で削除
+            for i in reversed(pop_queue):
+                spots.pop(i)
             if sunspot:    
                 #黒点とみなされない点だけで円を作成
                 cx,cy,r=fit_circle(np.array(spots, dtype=float))
@@ -144,5 +148,5 @@ if __name__== "__main__":
     picpath=askopenfilename(title="画像を選択してください", filetypes=[("Image files", "*.jpg;*.jpeg;*.png;*.tiff")])
     from time import time
     start = time()
-    print("cx,cy,r=",MIN2_ignore_sunspots(cv2.imread(picpath,0),filter_times=2,show=False,debug=True))
+    print("cx,cy,r=",MIN2_ignore_sunspots(cv2.imread(picpath,0),filter_times=2,show=True,debug=True))
     print(f"処理時間:{time()-start}秒")
